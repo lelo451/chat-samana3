e1de3a13c790ac98e07319dde3c721018f07a67a
Weslley Felipe <83655316+Weslley03@users.noreply.github.com>
2021-08-23 09:49:41 -0300

    Add files via upload

---
+++ Aplicação.dpr
@@ -0,0 +1,22 @@
+program Aplicação;
+
+uses
+  Vcl.Forms,
+  uFrmPrincipal in 'uFrmPrincipal.pas' {FrmPrincipal},
+  uFrmCadastrar in 'uFrmCadastrar.pas' {FrmCadastrar},
+  uDataModule01 in 'uDataModule01.pas' {DataModule01: TDataModule},
+  uFrmLogin in 'uFrmLogin.pas' {FrmLogin},
+  uFrmChat in 'uFrmChat.pas' {FrmChat};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmPrincipal, FrmPrincipal);
+  Application.CreateForm(TFrmCadastrar, FrmCadastrar);
+  Application.CreateForm(TDataModule01, DataModule01);
+  Application.CreateForm(TFrmLogin, FrmLogin);
+  Application.CreateForm(TFrmChat, FrmChat);
+  Application.Run;
+end.
---
+++ uFrmLogin.pas
@@ -0,0 +1,70 @@
+unit uFrmLogin;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, uFrmChat;
+
+type
+  TFrmLogin = class(TForm)
+    Label1: TLabel;
+    EdEmail: TEdit;
+    EdApelido: TEdit;
+    BtEntrar: TButton;
+    procedure BtEntrarClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmLogin: TFrmLogin;
+
+implementation
+
+{$R *.dfm}
+
+uses uDataModule01;
+
+procedure TFrmLogin.BtEntrarClick(Sender: TObject);
+begin
+
+  if (EdEmail.Text) <> '' then
+  begin
+    DataModule01.QueryUsuario.SQL.Clear;
+    DataModule01.QueryUsuario.SQL.Add('SELECT EmailUsu FROM chatusuario');
+    DataModule01.QueryUsuario.SQL.Add('WHERE EmailUsu = '+QuotedStr(Trim(EdEmail.Text)) );
+    DataModule01.QueryUsuario.Open;
+    if not DataModule01.QueryUsuario.IsEmpty then
+    begin
+      FrmChat.Apelido := EdApelido.Text;
+      FrmChat.Email := EdEmail.Text;
+      FrmChat.Show;
+    end
+  end
+  else
+  begin
+    MessageDlg('Usuario inválido.', MtError, [MbOK], 0);
+  end;
+
+  if (EdEmail.Text = '') AND (EdApelido.Text = '') then
+  begin
+    MessageDlg('Campos vazios.', MtError, [MbOK], 0);
+  end;
+
+   with DataModule01.TableUsuario do
+  begin
+
+    Close;
+    SQL.Clear;
+    SQL.Add('UPDATE chatusuario SET Online = 1 WHERE EmailUsu =:pnom ');
+    ParamByName('pnom').AsString := EdEmail.Text;
+    ExecSQL;
+
+  end;
+
+end;
+
+end.
